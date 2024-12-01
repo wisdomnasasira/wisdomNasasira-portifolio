1112b5a8a797eeee91a79e2cc105b43e314e1c6c
wisdomnasasira <122895015+wisdomnasasira@users.noreply.github.com>
2024-12-01 14:46:55 +0300

    Add files via upload

---
+++ script.js
@@ -0,0 +1,6 @@
+// script.js
+document.querySelector('form').addEventListener('submit', function(e) {
+    e.preventDefault(); // Prevent the default form submission
+    alert('Thank you for contacting me!'); // Alert on form submission
+});
+
